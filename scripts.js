401c30019f06a38237ef813724691efdfbfbb18c
nmsj21 <forsosialmed@gmail.com>
2024-09-10 20:05:24 +0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-function hide_question() {
+function show_question() {
     document.getElementById('lang-box').style.display = 'flex';
+}
+function show_links() {
+    document.getElementById('menu-links').style.display = 'flex';
+    document.getElementById('menu-link-close').style.display = 'flex';
+    document.getElementById('menu-link-bar').style.display = 'none';
+    document.getElementById('top-bar').style.width = '150px';
+}
+
+function hide_links() {
+    document.getElementById('menu-links').style.display = 'none';
+    document.getElementById('menu-link-close').style.display = 'none';
+    document.getElementById('menu-link-bar').style.display = 'flex';
 }
